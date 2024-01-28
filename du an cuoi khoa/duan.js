2beee2e8a1121ef82333a8aeb7131db35d365806
Nguyễn Tiến Đạt <ADMIN@TIENDAT-ASU>
2024-01-28 09:50:51 +0700

    next

--- du an cuoi khoa/duan.js
+++ du an cuoi khoa/duan.js
@@ -41,7 +41,7 @@ log.addEventListener("click", function () {
 
 const checkuser = localStorage.getItem("user");
 const dataUser = JSON.parse(checkuser);
-name_user.innerHTML = dataUser.email;
+name_user.innerHTML = dataUser?.email;
 
 if (checkuser) {
   login.style.display = "none";
@@ -60,21 +60,21 @@ if (checkuser) {
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
